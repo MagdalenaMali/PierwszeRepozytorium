2f8e6475518d0af3d8d131b31bde02a09749d4a4
MagdalenaMali <magda.maliszewska08@gmail.com>
2019-11-24 08:08:34 +0100

    Add infinity ahile to app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1.Dodaj Liczby");
+                Console.WriteLine("2.Odejmij liczby");
+                Console.WriteLine("3. * liczby");
+                Console.WriteLine("4. Dziel liczby");
+                Console.WriteLine("5. Wyjście");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
